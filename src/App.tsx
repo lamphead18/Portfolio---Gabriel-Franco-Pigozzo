6a6afd4a9f079e835c28fae405c871bf1ed17098
gabrielfpigozzo@gmail.com <gabrielfpigozzo@gmail.com>
2023-12-17 14:12:20 -0300

    First Configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css'
+import React from 'react';
+import './App.css';
 
-function App() {
-
+const App: React.FC = () => {
   return (
-    <>
-    </>
-  )
+    <div className="app">
+      <div className="info-box">
+        <h2>Gabriel Franco Pigozzo</h2>
+        <p>Web Development - FullStack</p>
+        <p>Ceará - Brazil</p>
+        <p>gabrielfpigozzo@gmail.com</p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
